ebeb4cd3cd46476eabcf6c227f01a3bad84936df
muzny <74148718+muznyahamed@users.noreply.github.com>
2023-12-16 10:38:31 +0530

    initial chat app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from langchain.llms import GooglePalm
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+google_api_key = "AIzaSyAGLqWWGWp5BcpHM9TDXTznVh2_ipOSxf4"
+llm = GooglePalm(google_api_key=google_api_key)
+llm.temperature = 0.1
 
 value ="enter your name "
 
@@ -18,7 +23,34 @@ with st.sidebar:
     )
 st.write(value)
 chat = st.chat_input("Say something")
-if chat:
-    chats.append(chat)
-    st.write(f"User has sent the following prompt: {chat}")
-    print(chats)
+import string
+import random
+
+
+def randon_string(user_input):
+    user_input =  user_input
+    return llm(user_input)
+
+
+def chat_actions():
+    st.session_state["chat_history"].append(
+        {"role": "user", "content": st.session_state["chat_input"]},
+    )
+
+    st.session_state["chat_history"].append(
+        {
+            "role": "assistant",
+            "content": randon_string(st.session_state["chat_input"]),
+        },
+    )
+
+
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
+
+st.chat_input("Enter your message", on_submit=chat_actions, key="chat_input")
+
+for i in st.session_state["chat_history"]:
+    with st.chat_message(name=i["role"]):
+        st.write(i["content"])
